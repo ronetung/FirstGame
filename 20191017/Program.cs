53a11a11287c8bb5fc3467f0a8353370d9dea737
User <User@DESKTOP-T154JUM>
2019-10-17 19:44:31 +0800

    123

---
+++ 20191017/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20191017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input=-1;
+            int guess=55;
+
+
+            while (true)
+            {
+                String input_str = Console.ReadLine();
+                input = int.Parse(input_str);
+
+
+                if (input==guess)
+                {
+                    Console.Write("恭喜你答對了!");
+                    break;
+
+                }
+
+                Console.Write("猜錯了，再來一次!");
+                   
+            }
+
+
+            Console.WriteLine("PAUSE");
+            Console.ReadKey();
+        }
+    }
+}
